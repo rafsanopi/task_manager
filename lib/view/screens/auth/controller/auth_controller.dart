8f2f896462f5ed7cc98f8909064041c4c20c746b
rafsanopi <rafsan3@dti.ac>
2024-09-06 22:11:42 +0600

    Init

---
+++ lib/view/screens/auth/controller/auth_controller.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class AuthenticationController extends GetxController {
+  ///========================= Sign In =========================
+
+  Rx<TextEditingController> emailController = TextEditingController().obs;
+  Rx<TextEditingController> passwordController = TextEditingController().obs;
+
+  ///========================= Sign Up =========================
+
+  Rx<TextEditingController> fullNameController = TextEditingController().obs;
+  Rx<TextEditingController> signupEmailController = TextEditingController().obs;
+  Rx<TextEditingController> phoneNumberController = TextEditingController().obs;
+  Rx<TextEditingController> signupPasswordController =
+      TextEditingController().obs;
+
+  Rx<TextEditingController> confirmPasswordController =
+      TextEditingController().obs;
+
+  Rx<TextEditingController> newPasswordController = TextEditingController().obs;
+
+  Rx<TextEditingController> oldPasswordController = TextEditingController().obs;
+
+  RxString activationCode = "".obs;
+}
